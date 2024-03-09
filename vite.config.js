4a5d65f548044a6842a16be9522c6b603e803d85
Mehdeddine Soltani <pro.mehdi.soltani@gmail.com>
2024-03-09 10:13:49 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Adjust the limit as needed
+  },
 })
